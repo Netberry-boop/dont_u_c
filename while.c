2a18254ab9b6203878f13cbb0f2db7455119f11e
Netberry-boop <yusuf.a.mail00@gmail.com>
2026-05-27 21:17:31 +0530

    while-loop

---
+++ while.c
@@ -0,0 +1,18 @@
+/* while.c */
+#include<stdio.h>
+
+int main()
+{
+
+ int x; 
+ while(x != 1)
+ {
+  // this part is executed iteratively until user enters and changes the "x" value to 1
+  printf("do you want to quit? enter 1 to quit from the program\n");
+  scanf("%d",&x);
+ }
+ return 0;
+}
+
+
+// to debug code -> ctrl+_ (to go to the line of error indicated by the compiler)

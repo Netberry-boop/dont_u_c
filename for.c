abc1061d33cc79ce9fa90b379b0f4445898d1a67
Netberry-boop <yusuf.a.mail00@gmail.com>
2026-05-30 21:39:24 +0530

    multiplication table & assert() function

--- for.c
+++ for.c
@@ -1,19 +1,24 @@
 /* for.c */
 
 #include<stdio.h>
-
+#include<assert.h> // contains the assert function 
 int main()
 {
  // syntax - for(current/initialization; condition; operation)
  //flow : current -> condition check -> operation at last 
  
- int num;
+ int table;
  printf("pick a number between 1-12: ");
- scanf("%d",&num);
- printf("%d's table :\n",num);
+ scanf("%d",&table);
+ 
+
+ assert((table < 13) && (table>0)); /*a simple way to do fault handling, 
+ if condition is true the program continues if not the program is terminated*/
+
+ printf("%d's table :\n",table);
  for(int i = 1; i<=10; i++)
  { 
-  printf("%d X %d = %d\n",num,i,(num*i));
+  printf("%d X %d = %d\n",table,i,(table*i));
  }
 
  return 0;

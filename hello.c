5827c6435f983bf025ff98d2293bee711e510ece
Netberry-boop <yusuf.a.mail00@gmail.com>
2026-06-01 21:51:27 +0530

    skeleton

--- hello.c
+++ hello.c
@@ -4,11 +4,11 @@
 /* function : is a named block of code that performs a specific task, it helps to divide program into smaller,
  reusable and organized parts. */
 
-int main() 
+int main()
 // funtions are identified by noticing paranthesis "()" directly connected to name
-// main function is always present in a c project  
+// main function is always present in a c project
 {
- printf("Hello World! \n");
- // "\n" this is new-line character it shifts cursor to the next line   
- return 0; // this means we are exiting the program(returning "0" also silently means that everything is ok)
+    printf("Hello World! \n");
+    // "\n" this is new-line character it shifts cursor to the next line
+    return 0; // this means we are exiting the program(returning "0" also silently means that everything is ok)
 }

173a84fd951a9e965995a9b64049266ad9ea95e2
Netberry-boop <yusuf.a.mail00@gmail.com>
2026-06-01 21:49:18 +0530

    area-circle

--- float.c
+++ float.c
@@ -1,20 +1,19 @@
 /* float.c */
 
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
- 
- float pi, radius, area; // float command to create float datatype 
- printf("what is the radius of the circle in cm?\n");
- scanf("%f", &radius); // "%f" is format string for float datatype
- /*
-  %s - string
-  %d - integer
-  %f - float 
- */ 
- pi = 3.14;
- area = pi * radius * radius;
- printf("the area of the circle is %f\n", area);
- return 0;
 
+    float pi, radius, area; // float command to create float datatype
+    printf("what is the radius of the circle in cm?\n");
+    scanf("%f", &radius); // "%f" is format string for float datatype
+    /*
+     %s - string
+     %d - integer
+     %f - float
+    */
+    pi = 3.14;
+    area = pi * radius * radius;
+    printf("the area of the circle is %f\n", area);
+    return 0;
 }

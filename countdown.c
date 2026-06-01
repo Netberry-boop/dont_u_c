665bb50b9b28378c7e5fce80755d4fc1428cdd6f
Netberry-boop <yusuf.a.mail00@gmail.com>
2026-06-01 21:40:28 +0530

    sleep() and fflush()

--- countdown.c
+++ countdown.c
@@ -1,18 +1,20 @@
 /* countdown.c */
 
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int main(){
-// application of sleep() and fflush() functions countdown of [10,9,8,7,6,5,4,3,2,1,0]
-    int count_max; 
-    printf("enter number from where the countdown should begin : ");    
-    scanf("%d",&count_max);
+int main()
+{
+    // application of sleep() and fflush() functions countdown of [10,9,8,7,6,5,4,3,2,1,0]
+    int count_max;
+    printf("enter number from where the countdown should begin : ");
+    scanf("%d", &count_max);
     printf("countdown begins ...");
     sleep(1);
     fflush(stdout);
     printf("\n");
-    while(count_max>=0){
+    while (count_max >= 0)
+    {
         sleep(1);
         printf("%d\n", count_max--);
     }

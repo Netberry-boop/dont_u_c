3c446d200c637e6434bd0953bff4e542e3664e97
Netberry-boop <yusuf.a.mail00@gmail.com>
2026-06-01 22:00:59 +0530

    switch cases

--- switch.c
+++ switch.c
@@ -1,16 +1,17 @@
-/* switch.c */ 
+/* switch.c */
 
 // demo if switch condition
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
 
- printf("Choose between 1. apples, 2. pears or 3. bananas\n");
- int choice;
- scanf("%d",&choice);
- switch(choice){
+	printf("Choose between 1. apples, 2. pears or 3. bananas\n");
+	int choice;
+	scanf("%d", &choice);
+	switch (choice)
+	{
 	case 1:
 		printf("apples are a good choice!!\n");
 		break;
@@ -22,8 +23,6 @@ int main()
 		break;
 	default:
 		printf("you didn't select from the provided options, exiting program ...\n");
- }
- return 0 ; 
+	}
+	return 0;
 }
-
-

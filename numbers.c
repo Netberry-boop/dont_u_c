a34ebc1c3c7526ed5ce80692ca536158db33360e
Netberry-boop <yusuf.a.mail00@gmail.com>
2026-06-01 21:55:09 +0530

    integers

--- numbers.c
+++ numbers.c
@@ -3,17 +3,14 @@
 
 int main()
 {
-  
- int x,y,answer;  // command to create an interger is int
- printf("select a number\n");
- scanf("%d", &x); // here "%d" is format string to represent integer
- printf("Select another number\n");
- scanf("%d", &y);
- answer = x + y ;
- printf("the answer is %d\n", answer);
- 
- return 0 ; 
 
+    int x, y, answer; // command to create an interger is int
+    printf("select a number\n");
+    scanf("%d", &x); // here "%d" is format string to represent integer
+    printf("Select another number\n");
+    scanf("%d", &y);
+    answer = x + y;
+    printf("the answer is %d\n", answer);
 
+    return 0;
 }
-

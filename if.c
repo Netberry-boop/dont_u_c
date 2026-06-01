2fb1a65cd6f00618c9b04581a24e59adce71f37d
Netberry-boop <yusuf.a.mail00@gmail.com>
2026-06-01 21:53:39 +0530

    speed - alert

--- if.c
+++ if.c
@@ -1,25 +1,24 @@
 /* if.c */
-#include<stdio.h> 
+#include <stdio.h>
 
 int main()
 {
 
- int speed; 
- printf("enter your speed in kmph\n");
- scanf("%d",&speed);
- if(speed >= 100)
- {
-  printf("you are are going very fast!! slow down to avoid any accidents!\n");
- }
- else if(speed>=50 && speed<100) 
- {
-  printf("your speed is perfect\n");
- } 
- else
- {
-  printf("you going slow!, speed up if you want to reach on time\n");
- }
- 
- return 0;	
+    int speed;
+    printf("enter your speed in kmph\n");
+    scanf("%d", &speed);
+    if (speed >= 100)
+    {
+        printf("you are are going very fast!! slow down to avoid any accidents!\n");
+    }
+    else if (speed >= 50 && speed < 100)
+    {
+        printf("your speed is perfect\n");
+    }
+    else
+    {
+        printf("you going slow!, speed up if you want to reach on time\n");
+    }
 
+    return 0;
 }

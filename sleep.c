379b9d18298ae7c18437b1ce9dd425f5b86ce155
Netberry-boop <yusuf.a.mail00@gmail.com>
2026-06-01 21:59:21 +0530

    fflush(stdout)

--- sleep.c
+++ sleep.c
@@ -1,40 +1,35 @@
 /* sleep.c */
 
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int main()
 
 {
- /* "sleep()" function is used for delaying the next step
- of the program */
- 
+    /* "sleep()" function is used for delaying the next step
+    of the program */
 
- printf("wait while processing");
- fflush(stdout);
- /* 
+    printf("wait while processing");
+    fflush(stdout);
+    /*
 
- "fflush()" usage is essential here as buffer will only output 
- if it sees the "\n"-newline character therefore if not used the sleep
- time is combined untill "\n" is seen cause inaccurate output
+    "fflush()" usage is essential here as buffer will only output
+    if it sees the "\n"-newline character therefore if not used the sleep
+    time is combined untill "\n" is seen cause inaccurate output
 
- */
+    */
 
- sleep(1);
- printf(".");
- fflush(stdout);
- sleep(1);
- printf(".");
- fflush(stdout);
- sleep(1);
- printf(".");
- fflush(stdout);
- sleep(1);
- printf("\nprocess completed\n");
+    sleep(1);
+    printf(".");
+    fflush(stdout);
+    sleep(1);
+    printf(".");
+    fflush(stdout);
+    sleep(1);
+    printf(".");
+    fflush(stdout);
+    sleep(1);
+    printf("\nprocess completed\n");
 
-
- return 0 ; 
-
-
-
+    return 0;
 }

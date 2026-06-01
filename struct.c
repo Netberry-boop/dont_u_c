c3ef47a1fc371173316f05a6662327b2ff7d2b29
Netberry-boop <yusuf.a.mail00@gmail.com>
2026-06-01 22:00:07 +0530

    custom datatype - composite

--- struct.c
+++ struct.c
@@ -1,15 +1,16 @@
-/* struct.c */ 
+/* struct.c */
 
-#include<stdio.h>
-#include<string.h> // this header file contains the "strcpy()" function
+#include <stdio.h>
+#include <string.h> // this header file contains the "strcpy()" function
 
-// it is composite of multiple variables 
+// it is composite of multiple variables
 // structure is made outside of main and then used inside main, it can also be used by other functions.
 
-struct person{
-	char title[8]; 
-	/* using multiple of 8 number of characters for memory efficiency 
-	- as in 64bit architecture memory blocks are of size 8 bytes and 
+struct person
+{
+	char title[8];
+	/* using multiple of 8 number of characters for memory efficiency
+	- as in 64bit architecture memory blocks are of size 8 bytes and
 	in 32bit architecture size of 4 bytes*/
 	char lastname[32];
 	int age;
@@ -18,17 +19,13 @@ struct person{
 int main()
 {
 
- struct person yusuf; //here the "struct person" represents the datatype, and yusuf is the variable name 
- // "strncpy(variable.element,"value",max_num_of_char)" function is used here to copy strings into the variable
- strncpy(yusuf.title, "yusuf", 7); // it is good practice to use (allocated-1) char in-order to prevent any error
- strncpy(yusuf.lastname, "ansari",31);
- yusuf.age = 21 ;
- 
- printf("%s %s is %d years old\n",yusuf.title,yusuf.lastname,yusuf.age);
+	struct person yusuf; // here the "struct person" represents the datatype, and yusuf is the variable name
+	// "strncpy(variable.element,"value",max_num_of_char)" function is used here to copy strings into the variable
+	strncpy(yusuf.title, "yusuf", 7); // it is good practice to use (allocated-1) char in-order to prevent any error
+	strncpy(yusuf.lastname, "ansari", 31);
+	yusuf.age = 21;
 
- return 0;
+	printf("%s %s is %d years old\n", yusuf.title, yusuf.lastname, yusuf.age);
 
+	return 0;
 }
-
- 
-		

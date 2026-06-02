e30898d6cb08403568102bae0806a1a47acfd6b7
Netberry-boop <yusuf.a.mail00@gmail.com>
2026-06-03 00:23:52 +0530

    sum_of_3_digit_even_natural - intent coding

---
+++ sum_of_3_digit_even_N.c
@@ -0,0 +1,30 @@
+/* sum_of_3_digit_even_N.c */
+
+#include<stdio.h>
+int num_digit(int num){
+    int count = 0;
+    while(num>0){
+        num=num/10;
+        ++count;
+    }
+    return count;
+}
+int is_even(int num){
+    if(num%2==0){
+        return 1;
+    }
+    return 0;
+} 
+
+int main(){
+    int num = 1; // start of natural numbers 
+    int sum = 0;
+    while(num_digit(num)>0 && num_digit(num)<4){
+        if(num_digit(num)==3&&is_even(num)){
+            sum+=num;
+        }
+        ++num;
+    }
+    printf("Sum of all 3-digit even natural number is : %d\n",sum);
+    return 0;
+}

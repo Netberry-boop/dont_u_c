bf6a27145cc2c65654e5e9f384031916297c4647
Netberry-boop <yusuf.a.mail00@gmail.com>
2026-06-04 19:08:16 +0530

    grading system - intent programming

--- disc_calc.c
+++ disc_calc.c
@@ -1,4 +1,15 @@
 /* disc_calc.c */
+/*Algorithm
+1) start
+2) initialize variables
+3) accept the total bill amount from the user
+4) if the bill amount is less than 2000 apply 5% dicount 
+5) if 2001<=totalcost<=5000 then apply 25% discount 
+6) if 5001<=totalcost<=10000 then apply 35% dicount
+7) if total cost exceeds 10000 then apply 50% dicount
+8) print the final payable amount
+9) stop
+*/
 
 #include<stdio.h> 
 
@@ -6,20 +17,20 @@ int main(){
     float tot_cost;
     printf("enter the total amount of the bill : ");
     scanf("%f",&tot_cost);
-    if(tot_cost>0&&tot_cost<2000){
+    if(tot_cost>=0&&tot_cost<2000){ //edge cases noticed - no discount for total_cost 2000, as condition is not mentioned in the question
         tot_cost-=tot_cost*0.05;
     }
-    else if(tot_cost>=2001&&tot_cost<=5000){
+    else if(tot_cost>=2001&&tot_cost<=5000){//edge cases noticed - no discount for float bill values between 2000-2001(ex:2000.5)
         tot_cost-=tot_cost*0.25;
     }
-    else if(tot_cost>=5001&&tot_cost<=10000){
+    else if(tot_cost>=5001&&tot_cost<=10000){//edge cases noticed - no discount for float bill values between 5000-5001(ex:5000.5) 
         tot_cost-=tot_cost*0.35;
     }
     else if(tot_cost>10000){
         tot_cost-=tot_cost*0.50;
     }
     else{
-        printf("Bill amount is not suitable for discounts/ invalid input\n");
+        printf("Bill amount is not suitable for discounts/ invalid input\n"); // for edge cases 
         // cases missed due to improper problem statement - 
         //(2000,2000-2000.9999,5000.1-5000.9999)
     }
--- grading.c
+++ grading.c
@@ -1,4 +1,17 @@
 /*grading.c*/
+/* Algorithm -
+1) start 
+2) initialize variables
+3) accept department confirmation from the user
+4) if user belongs to computer science department, proceed otherwise terminate program
+5) accept marks from user
+6) if marks > 90 print grade A
+7) if 70<=marks<=90  print grade B 
+8) if 50<=marks<=69  print grade C
+9) if marks<50 print grade D
+10) if marks input invalid - exit
+11)stop
+*/
 
 #include<stdio.h> 
 
@@ -10,7 +23,7 @@ int main() {
         int marks;
         printf("Please enter your marks : ");
         scanf("%d",&marks);
-        if(marks > 90){
+        if(marks > 90){ // here the cap to marks is unspecified (Max marks is unknown)
             printf("your grade is : %c\n",'A');
         }
         else if(marks>=70 && marks<=90){
@@ -19,7 +32,7 @@ int main() {
         else if(marks>=50 && marks<=69){
             printf("your grade is : %c\n",'C');
         }
-        else if(marks < 50){
+        else if(marks < 50){ // here the cap to marks is unspecified (Min marks is unknown(negative?))
             printf("your grade is : %c\n",'D');
         }
         else{
@@ -27,7 +40,7 @@ int main() {
             return 0;
         }
     }
-    else if(dep_check == 'N'||dep_check == 'n'){
+    else if(dep_check == 'N'||dep_check == 'n'){ // input from student only in the cs department 
         printf("sorry! this grading system is only for computer science department\n");
     }
     else{
